68996a16ddc6bd63f03d3742af119b4f27a5c252
AlwaysSylva <jonny.1000@gmail.com>
2017-10-21 03:56:16 +0100

    Refactored test case to use relative url using Django test client

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,32 @@
-from pytest_bdd import scenarios, when, then, parsers
+import os
+import sys
+import pytest
+import django
+
+from pytest_bdd import parsers, scenarios, then, when
+from splinter import Browser
+from django.test.utils import setup_test_environment
+
+sys.path.insert(0, "../")
+from manage import DEFAULT_SETTINGS_MODULE
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
+django.setup()
+setup_test_environment()
 
 scenarios('features')
 
-@when(parsers.parse('I browse to {site}'))
-def go_to_url(site, browser):
-    browser.visit(site)
 
-@then('I see the login page')
-def is_login_page(browser):
-    browser.find_by_css('.form-signin').first
+@pytest.fixture
+def django_browser():
+    return Browser('django')
+
+
+@when(parsers.parse('I browse to {url}'))
+def go_to_url(url, django_browser):
+    django_browser.visit(url)
+
+
+@then('I see the login form')
+def is_login_page(django_browser):
+    django_browser.find_by_css('.form-signin').first
